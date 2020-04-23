e4b2d63ae0d03f444418c00c9ecfe905a5d1b06b
Brendan Burke <bburke2016@gmail.com>
2020-04-23 12:11:10 -0400

    Update and rename JenkinsPRbuilder.md to JenkinsPRbuilder.groovy
    Update groovy pseudocode prepping for interviews and vet efforts

---
+++ JenkinsPRbuilder.groovy
@@ -0,0 +1,13 @@
+
+node("master") {
+
+//Define the pipeline parameters ideally the parameters have identical inputs
+def jobparameterss = []
+
+//Step 1 : Create local merge of Dev and PR Branch
+
+//Step 2 : Deploy the environemnt and run automated tests using local branch
+
+//Step 3 : Clean up environments and publish reports (ideally to slack)
+
+}
